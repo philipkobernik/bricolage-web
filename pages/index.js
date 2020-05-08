6a3af44d5f0710d6d95f0ae398b0a239c819faa3
Philip Kobernik <ptk921@gmail.com>
2020-05-07 23:42:26 -0700

    delete old index, rename i.js to index

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
 import firebase from 'firebase/app'
 import 'firebase/storage'
 import 'firebase/auth'
 import 'firebase/firestore'
 import 'isomorphic-unfetch'
 import clientCredentials from '../credentials/client'
+
+import ListMessages from '../components/ListMessages'
+import CurrentUser from '../components/CurrentUser'
 
 export async function getServerSideProps({ req, query }) {
   const user = req.session && req.session.decodedToken ? req.session.decodedToken : null
@@ -20,7 +23,7 @@ export async function getServerSideProps({ req, query }) {
   }
 }
 
-export default class Index extends Component {
+class Index extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -30,11 +33,6 @@ export default class Index extends Component {
       img: '',
       imgDownLoaded: true,
     }
-
-    this.addDbListener = this.addDbListener.bind(this)
-    this.removeDbListener = this.removeDbListener.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   uploadImg = e => {
@@ -59,35 +57,9 @@ export default class Index extends Component {
   }
 
   componentDidMount() {
+    console.log("index cDM");
     firebase.initializeApp(clientCredentials)
-
-    if (this.state.user) this.addDbListener()
-
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        this.setState({ user: user })
-        return user
-          .getIdToken()
-          .then(token => {
-            // eslint-disable-next-line no-undef
-            return fetch('/api/login', {
-              method: 'POST',
-              // eslint-disable-next-line no-undef
-              headers: new Headers({ 'Content-Type': 'application/json' }),
-              credentials: 'same-origin',
-              body: JSON.stringify({ token }),
-            })
-          })
-          .then(res => this.addDbListener())
-      } else {
-        this.setState({ user: null })
-        // eslint-disable-next-line no-undef
-        fetch('/api/logout', {
-          method: 'POST',
-          credentials: 'same-origin',
-        }).then(() => this.removeDbListener())
-      }
-    })
+    this.addDbListener() // initializes firestore
   }
 
   addDbListener() {
@@ -107,70 +79,22 @@ export default class Index extends Component {
     this.setState({ unsubscribe })
   }
 
-  removeDbListener() {
-    // firebase.database().ref('messages').off()
-    if (this.state.unsubscribe) {
-      this.state.unsubscribe()
-    }
-  }
-
-  handleChange(event) {
-    this.setState({ value: event.target.value })
-  }
-
-  handleSubmit(event) {
-    event.preventDefault()
-    var db = firebase.firestore()
-    const date = new Date().getTime()
-    db.collection('messages')
-      .doc(`${date}`)
-      .set({
-        id: date,
-        text: this.state.value,
-      })
-    this.setState({ value: '' })
-  }
-
-  handleLogin() {
-    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
-  }
-
-  handleLogout() {
-    firebase.auth().signOut()
-  }
-
   render() {
     const { user, value, messages } = this.state
 
     return (
       <div>
         <h1><em>bricolage</em></h1>
-        {user ? (
-          <button onClick={this.handleLogout}>Logout</button>
-        ) : (
-          <button onClick={this.handleLogin}>Login</button>
-        )}
-        {user && (
-          <div>
-            <form onSubmit={this.handleSubmit}>
-              <input
-                type={'text'}
-                onChange={this.handleChange}
-                placeholder={'add message...'}
-                value={value}
-              />
-            </form>
-            <ul>
-              {messages &&
-                Object.keys(messages).map(key => (
-                  <li key={key}>{messages[key].text}</li>
-                ))}
-            </ul>
-            <input accept='image/*' multiple type='file' onChange={this.uploadImg} ref={ el => this.fileInput = el }/>
-            <img style={{display:'block'}} src={this.state.img} />
-          </div>
-        )}
+
+        {firebase.apps.length > 0 && // whats the right way to do this?
+        <>
+          <CurrentUser fb={firebase} />
+          <ListMessages fb={firebase} />
+        </>
+        }
       </div>
     )
   }
 }
+
+export default Index;
